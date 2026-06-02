ad512ca21c2da75f94ddc5961562070968608c74
ChikatiAmulya <amulyachikati1@gmail.com>
2026-06-02 11:28:50 +0530

    Add script to move .jpg files to a new folder

---
+++ Task_Automation.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+source_folder = "images"
+destination_folder = "moved_images"
+
+if not os.path.exists(destination_folder):
+    os.makedirs(destination_folder)
+
+for file_name in os.listdir(source_folder):
+    if file_name.endswith(".jpg"):
+        source_path = os.path.join(source_folder,file_name)
+        destination_path = os.path.join(destination_folder,file_name)
+        shutil.move(source_path,destination_path)
+        print(file_name,"moved successfully")
+print("All .jpg files have been moved.")
